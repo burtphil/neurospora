9b5ae095fbd39b5e48af4b94454b9124c91ad1a6
Philipp Burt <burt@wiener.itb.pri>
2017-07-20 13:10:37 +0200

    added a test sample that can be used withe pytest package

---
+++ test_sample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 15:58:54 2017
+
+@author: burt
+"""
+
+
+
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 5
+
