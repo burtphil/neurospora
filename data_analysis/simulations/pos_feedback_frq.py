29023bf602bb723155a4798a1889901246827ad4
burtphil <philippburt@gmail.com>
2017-11-22 15:40:39 +0100

    changed plot styles

--- data_analysis/simulations/pos_feedback_frq.py
+++ data_analysis/simulations/pos_feedback_frq.py
@@ -8,12 +8,10 @@ This temporary script file is located here:
 ### import packages
 
 import numpy as np
-
-import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from scipy.signal import argrelextrema
 import string
+
 
 ####### implement biological model Hong et al 2008
 
@@ -102,7 +100,7 @@ state0 = [frq_mrna0,
 
 ### set time to integrate
 
-t      = np.arange(0,500,0.1)
+t      = np.arange(0,120,0.1)
 
 ### what is a proper time resolution?
 
@@ -110,92 +108,53 @@ t      = np.arange(0,500,0.1)
 state = odeint(clock,state0,t,args=(rate,))  
 
 ### plot all ODEs
-state_names = ["frq mRNA","$FRQ_c$","$FRQ_n$","wc-1 mRNA","$WC$-$1_c$",
-               "$WC$-$1_n$","$FRQ_n$:$WC$-$1_n$"]
+state_names = ["frq mRNA","FRQc","FRQn","wc-1 mRNA","WC-1c","WC-1n","FRQn:WC-1n"]
 
+pos_frq = state[:,1] / (1+ state[:,1])
 
-ylabel = "FRQc [a.u.]"
-plt.figure(figsize=(12,9))
-ax = plt.subplot(111)
-ax.plot(t,state)
-ax.set_xlabel("time (h)", fontsize= 'xx-large')
-ax.set_ylabel("concentrations (a.u.)", fontsize= 'xx-large')
-#plt.xticks(np.arange(0, 49, 12.0))
-plt.legend(state_names,loc='upper right')
-
-ax.tick_params(labelsize= 'x-large')
-ax.text(-0.05, .97, "A", transform=ax.transAxes, 
-        size=20, weight='bold')
-plt.tight_layout()
-plt.show()
-plt.savefig("simulation1.pdf",dpi=1200)
-
-###
-plt.figure(figsize=(9,12))
-ax = plt.subplot(3,1,1)
-ax.plot(t, state[:,4],"purple")
-#plt.xlabel("time [h]", fontsize= 'xx-large')
-ax.set_ylabel('$WC$-$1_c$', fontsize= 'xx-large')
-ax.tick_params(labelsize= 'x-large')
-ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-ax.set_yticks([0.0312,0.0316,0.0320,0.0324])
-#plt.xticks(np.arange(0, 49, 12.0))
-#plt.title('WC-1c')
-
-ax1 = plt.subplot(3,1,2)
-ax1.plot(t, state[:,2],"g")
-#plt.xlabel("time [h]", fontsize= 'xx-large')
-ax1.set_ylabel('$FRQ_n$', fontsize= 'xx-large')
-ax1.tick_params(labelsize= 'x-large')
-ax1.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-ax1.set_yticks([0.025,0.075,0.125])
-#plt.xticks(np.arange(0, 49, 12.0))
-#plt.title('FRQn')
-
-ax2 = plt.subplot(3,1,3)
-ax2.plot(t, state[:,6],"pink")
-ax2.set_xlabel("time (h)", fontsize= 'xx-large')
-ax2.set_ylabel('$FRQ_n:WC$-$1_n$', fontsize= 'xx-large')
-ax2.tick_params(labelsize= 'x-large')
-ax2.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-ax2.set_yticks([0.1,0.15,0.2])
-
-axes = [ax,ax1,ax2]
-
-for n, ax in enumerate(axes): 
-    ax.text(-0.11, .97, string.ascii_uppercase[n+1], transform=ax.transAxes, 
-            size=20, weight='bold')
-#plt.xticks(np.arange(0, 49, 12.0))
-#plt.title('FRQn:WC-1n')
-#plt.savefig("simulation2.pdf",dpi=1200)
-plt.tight_layout()
-plt.savefig("simulation2.pdf",dpi=1200)
-plt.show()
-
-"""
 def style(axes):
     for ax in axes:
-        ax.tick_params(labelsize="x-large")
-        ax.set_ylabel("$FRQ_c$", fontsize = "xx-large")
+        ax.set_xlabel("time (h)", fontsize = 'xx-large')
+        ax.set_ylabel("$FRQ_c:(K+FRQ_c)$", fontsize = 'xx-large')
+        ax.set_xticks([0,60,120])
+        ax.tick_params(labelsize= 'x-large')
 
-fig = plt.figure(figsize=(12,9))
+fig, axes = plt.subplots(1,3,figsize = (12,4))
 
-ax = plt.subplot2grid((3, 2), (0, 0), rowspan=3)
-ax2 = plt.subplot2grid((3, 2), (0, 1))
-ax3 = plt.subplot2grid((3, 2), (1, 1))
-ax4 = plt.subplot2grid((3, 2), (2, 1))
+axes.flatten()
+ax = axes[0]
+ax1 = axes[1]
+ax2 = axes[2]
 
-ax.plot(t,state)
-ax.legend(state_names,loc='upper right')
-ax2.plot(t, state[:,4],"purple")
-ax3.plot(t, state[:,2],"g")
-ax4.plot(t, state[:,6],"pink")
+ax.plot(t,pos_frq, 'k')
+ax.set_ylim(0.9,1)
+ax.set_yticks([0.9,0.95,1])
 
-axes = [ax2,ax3,ax4]
+ax1.plot(t, pos_frq, 'k')
+#ax.set_ylim(0,2)
+ax1.set_yticks([0,1,2])
 
-style(axes)
+ax2.plot(state[:,1],pos_frq, 'k')
+ax2.set_xlabel("$FRQ_c$", fontsize = "xx-large")
+ax2.set_ylabel("$FRQ_c:(K+FRQ_c)$", fontsize = 'xx-large')
+ax2.set_yticks([0,1,2])
+ax2.set_ylim(0,2)
+ax2.set_xticks([15,25,35])
+ax2.tick_params(labelsize="x-large")
 
+style([ax,ax1])
+
+ax.text(-0.29, .98, "A", transform=ax.transAxes, size=20, weight='bold')
+axes = [ax1,ax2]
 for n, ax in enumerate(axes): 
-    ax.text(-0.13, .97, string.ascii_uppercase[n], transform=ax.transAxes, 
+    ax.text(-0.19, .98, string.ascii_uppercase[n+1], transform=ax.transAxes, 
             size=20, weight='bold')
-"""
+
+
+    
+#ax1.text(1, .98, "B", transform=ax.transAxes, size=20, weight='bold')
+#ax2.text(2.3, .98, "C", transform=ax.transAxes, size=20, weight='bold')
+
+plt.tight_layout()
+fig.savefig("pos_feedback_FRQ.pdf",dpi=1200)
+plt.show()
